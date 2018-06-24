40175a49dfd06ac4557f5e73ce9c3bcb7e454fcf
Amit Chaudhary <camitiw@gmail.com>
2018-06-24 22:20:38 +0545

    Integrate todoist habits into the productivity score

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import os
 import requests
 from flask import Flask
+import pickle
+from todoist.api import TodoistAPI
+import re
+from datetime import datetime, timedelta
 
 app = Flask(__name__)
 
@@ -24,14 +28,75 @@ def get_pulse_change():
     resp = requests.get(url, params=params)
     json_response = resp.json()
     latest, previous, *_ = json_response
-    change = latest['productivity_pulse'] - previous['productivity_pulse']
+    rescuetime_change = latest['productivity_pulse'] - previous['productivity_pulse']
+    done, total_habits = get_streak()
+    streak_change = get_streak_change(done)
+    change = rescuetime_change/100.0 * (100-total_habits) + streak_change
+    change = round(change, 1)
     return "{:+}%".format(change)
+
+
+def get_token():
+    token = os.getenv('TODOIST_APIKEY')
+    return token
+
+
+def is_habit(text):
+    return re.search(r'\[day\s(\d+)\]', text)
+
+
+def is_today(text):
+    today = (datetime.utcnow() + timedelta(1)).strftime("%a %d %b")
+    return text[:10] == today
+
+def is_due(text):
+    yesterday = datetime.utcnow().strftime("%a %d %b")
+    return text[:10] == yesterday
+
+
+def get_streak():
+    todoist_token = get_token()
+    api = TodoistAPI(todoist_token)
+    api.sync()
+    tasks = api.state['items']
+    total = 0
+    done = 0
+    for task in tasks:
+        if (task['due_date_utc']) and is_habit(task['content']):
+            if(is_today(task['due_date_utc']) or is_due(task['due_date_utc'])):
+                habit = is_habit(task['content'])
+                streak = int(habit.group(1))
+                if streak:
+                    done += 1
+                total += 1
+    return done, total    
+
+def get_streak_change(streak):
+    fp = open('data.pkl', 'rb')
+    details = pickle.load(fp)
+    fp.close()
+    last_done = details['habits']
+    change = streak - last_done
+    details = {'habits': streak}
+    fp = open('data.pkl', 'wb')
+    pickle.dump(details, fp)
+    fp.close()
+    return change
+
 
 @app.route('/')
 def index():
     change = get_pulse_change()
-    send_notification(change)
+    # send_notification(change)
     return change
+
+
+@app.route('/reset')
+def reset():
+    details = {'habits': 0}
+    pickle.dump(details, open('data.pkl', 'wb'))
+    return 'Done.'
+
 
 if __name__ == '__main__':
     app.run()
