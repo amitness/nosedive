d945ba72264d4d76754a507822196950071d6a8d
Amit Chaudhary <meamitkc@gmail.com>
2020-01-01 22:53:50 +0545

    Migrate to github actions

--- nosedive.py
+++ nosedive.py
@@ -1,18 +1,15 @@
 import os
 import requests
-from flask import Flask
-
-app = Flask(__name__)
 
 def send_notification(message):
     pushover_token = os.getenv('PUSHOVER_TOKEN')
     pushover_url = 'https://api.pushover.net/1/messages.json'
     params = {
         'token': pushover_token,
-        'user': 'uga9w2s6wJsnGUwTjpmJnyMQnV6E5q',
+        'user': os.getenv('PUSHOVER_USER_KEY'),
         'priority': -1,
         'message': message,
-        'title': 'Productivity'
+        'title': message
     }
     requests.post(pushover_url, params=params)
 
@@ -25,13 +22,9 @@ def get_pulse_change():
     json_response = resp.json()
     latest, previous, *_ = json_response
     change = latest['productivity_pulse'] - previous['productivity_pulse']
-    return "{:+}%".format(change)
-
-@app.route('/')
-def index():
-    change = get_pulse_change()
-    send_notification(change)
-    return change
+    return "{}%".format(abs(change))
 
 if __name__ == '__main__':
-    app.run()
+    change = get_pulse_change()
+    send_notification(change)
+    print(change)
